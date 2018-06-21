575da2a0c4d182cb4fda1c0dcde88b5622569559
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-06-21 19:12:12 +0300

    Add Employee page form submit to function mySubmit

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './../../Employee';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
+  employee: Employee = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    country: '',
+    city: '',
+    phone: 0,
+    salary: 0
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+
+  mySubmit({value, valid}: {value: Employee, valid: boolean}) {
+    if (!valid) {
+      console.log('not correct data');
+    } else {
+      console.log(this.employee);
+    }
   }
 
 }
