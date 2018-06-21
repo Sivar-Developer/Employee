d025b97a6326fdb2a8b6e26daefec97e195e6482
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-06-21 17:22:13 +0300

    Show and calculate data

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -10,6 +10,8 @@ import { Employee } from './../../Employee';
 export class EmployeesComponent implements OnInit {
 
   employees: Employee[];
+  totalEmployees: number;
+  totalSalarySum: number;
 
   constructor(public employeeService: EmployeeService) { }
 
@@ -17,7 +19,21 @@ export class EmployeesComponent implements OnInit {
     this.employeeService.getEmployees().valueChanges().subscribe(employees => {
         this.employees = employees;
         console.log(this.employees);
+        this.getTotalEmployees();
     });
+  }
+
+  getTotalEmployees() {
+    let total = 0;
+    let totalSalary = 0;
+    for (let index = 0; index < this.employees.length; index++) {
+      total += 1;
+      totalSalary += parseFloat(this.employees[index].salary.toString());
+    }
+    this.totalEmployees = total;
+    this.totalSalarySum = totalSalary;
+    console.log(this.totalEmployees);
+    console.log(this.totalSalarySum);
   }
 
 }
